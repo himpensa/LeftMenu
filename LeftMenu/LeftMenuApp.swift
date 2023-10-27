6ecc1aa11dedebb0fb6f49305083abe01296c79d
Antoine Himpens <himpensa@gmail.com>
2023-10-27 21:52:55 +0200

    Initial Commit

---
+++ LeftMenu/LeftMenuApp.swift
@@ -0,0 +1,17 @@
+//
+//  LeftMenuApp.swift
+//  LeftMenu
+//
+//  Created by Antoine Himpens on 27/10/2023.
+//
+
+import SwiftUI
+
+@main
+struct LeftMenuApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
